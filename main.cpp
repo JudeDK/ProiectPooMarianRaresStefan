d782b69bfb561793d4e6d0a8ed5c1d5293a6e0c3
JudeDK <raresmarian3344@gmail.com>
2023-11-22 20:40:06 +0200

    ok

--- main.cpp
+++ main.cpp
@@ -1,264 +1,216 @@
 #include <iostream>
 #include <string>
-#include <vector>
-//#include <algorithm>
-
-using namespace std;
-
 class Materie {
 private:
-    string nume;
-    string profesor;
+    std::string name;
+    std::string teacher;
 
 public:
-    ~Materie(){cout<<"A fost apelat \n";}
+    ~Materie(){std::cout<<"A fost apelat \n";}
 
-    explicit Materie(const string& name = "Fizica", const string& profesor = "Popescu") {
-        this->nume = name;
-        this->profesor = profesor;
+    explicit Materie(const std::string& local_name = "Fizica", const std::string& local_teacher = "Popescu") {
+        this->name = local_name;
+        this->teacher = local_teacher;
     }
 
-    string getnume() const {
-        return this->nume;
+    std::string getname() const {
+        return this->name;
     }
 
-    void setnume(const string &name) {
-        this->nume = name;
+    void setname(const std::string &local_name) {
+        this->name = local_name;
     }
 
-    string getprofesor() const {
-        return this->profesor;
+    std::string getteacher() const {
+        return this->teacher;
     }
 
-    void setprofesor(const string &prof) {
-        this->profesor = prof;
+    void setteacher(const std::string &local_teacher) {
+        this->teacher = local_teacher;
     }
 
-    friend ostream &operator<<(ostream &out, const Materie &m);
-
-    friend istream &operator>>(istream &, Materie&);
+    friend std::ostream &operator<<(std::ostream &out, const Materie &m);
+    friend std::istream &operator>>(std::istream &, Materie&);
 
 };
-istream  &operator>>(istream &is,  Materie &m) {
-    cout << "Materia este: " ;
-    is >> m.nume;
-    cout << "Materia este predata de: ";
-    is >> m.profesor;
+std::istream  &operator>>(std::istream &is,  Materie &m) {
+    std::cout << "Materia este: " ;
+    is >> m.name;
+    std::cout << "Materia este predata de: ";
+    is >> m.teacher;
     return is;
 }
-ostream &operator<<(ostream &out, const Materie &m) {
-    out << "Materia este: " << m.nume << endl;
-    out << "Materia este predata de: " << m.profesor << endl;
+std::ostream &operator<<(std::ostream &out, const Materie &m) {
+    out << "Materia este: " << m.name << std::endl;
+    out << "Materia este predata de: " << m.teacher << std::endl;
     return out;
 }
 class Student {
 private:
-    string nume;
-    string cnp;
-    double medie{};
-    vector <Student>St;
+    std::string name;
+    std::string cnp;
+    double average{};
 public:
     Student()=default;
-    static bool is_valid_cnp(const string &cnp) {
-        return cnp.length() == 13 && cnp.find_first_not_of("0123456789") == string::npos;
+    static bool is_valid_cnp(const std::string &local_cnp) {
+        return local_cnp.length() == 13 && local_cnp.find_first_not_of("0123456789") == std::string::npos;
     }
 
-    Student(const string &nume , const string &cnp,double medie) {
-        this->nume = nume;
+    Student(const std::string &local_name , const std::string &local_cnp,double average) {
+        this->name = local_name;
         if (!is_valid_cnp(cnp)) {
-            cout << "CNP invalid! CNP-ul trebuie sa aiba 13 cifre." << endl;
+            std::cout << "CNP invalid! CNP-ul trebuie sa aiba 13 cifre." << std::endl;
         }
-        this->cnp = cnp;
-        if (medie >= 0 && medie <= 10) {
-            this->medie = medie;
+        this->cnp = local_cnp;
+        if (average >= 0 && average <= 10) {
+            this->average = average;
         } else {
-            cout << "Medie invalida! Medie trebuie sa fie intre 0 si 10." << endl;
+            std::cout << "Medie invalida! Medie trebuie sa fie intre 0 si 10." << std::endl;
         }
     }
-    string getnume() const {
-        return this->nume;
+    std::string getname() const {
+        return this->name;
     }
-    void setnume(const string &name) {
-        this->nume = name;
+    void setname(const std::string &local_name) {
+        this->name = local_name;
     }
-    string getcnp() const {
+    std::string getcnp() const {
         return this->cnp;
     }
-    bool setcnp(const string &cenepe) {
-        if (is_valid_cnp(cenepe)) {
-            this->cnp = cenepe;
+    bool setcnp(const std::string &local_cnp) {
+        if (is_valid_cnp(local_cnp)) {
+            this->cnp = local_cnp;
             return true;  // CNP valid
         } else {
-            cout << "CNP invalid! CNP-ul trebuie sa aiba 13 cifre." << endl;
+            std::cout << "CNP invalid! CNP-ul trebuie sa aiba 13 cifre." << std::endl;
             return false;  // CNP invalid
         }
     }
-    double getmedie() const {
-        return medie;
+    double getaverage() const {
+        return average;
     }
-    bool setmedie(double med) {
-        if (medie >= 0 && med <= 10) {
-            this->medie = med;
+    bool setaverage(double med) {
+        if (average >= 0 && med <= 10) {
+            this->average = med;
             return true;
         } else {
-            cout << "Medie invalida! Medie trebuie sa fie intre 0 si 10." << endl;
+            std::cout << "Medie invalida! Medie trebuie sa fie intre 0 si 10." << std::endl;
             return false;
         }
     }
-    explicit Student(const vector<Student>&localstudenti){
-        this->St=localstudenti;
-    }
-
-   /* static bool comparator(const Student& s1, const Student& s2) {
-        if (s1.getmedie()==s2.getmedie()) {
-            if (s1.getnume() == s2.getnume()) {
-                return s1.getcnp() < s2.getcnp();
-            }
-
-            return s1.getnume() <s2.getnume();
-        }
-
-        return s1.getmedie() > s2.getmedie();
-    }*/
-    friend ostream &operator<<(ostream &out, const Student &st);
-    friend istream &operator>>(istream &is, Student &st);
-    Student& operator=(const Student& st){
-        this->nume=st.nume;
-        this->cnp=st.cnp;
-        this->medie=st.medie;
-        return *this;
-    }
-    ~Student() { cout << "A fost apelat" << endl; }
+    friend std::ostream &operator<<(std::ostream &out, const Student &st);
+    friend std::istream &operator>>(std::istream &is, Student &st);
+    ~Student() { std::cout << "A fost apelat" << std::endl; }
 };
-ostream &operator<<(ostream &out, const Student &st) {
-    out << "Studentul este: " << st.nume << endl;
-    out << "Studentul are cnp: " << st.cnp << endl;
-    out << "Studentul are media: "<<st.medie<<endl;
+std::ostream &operator<<(std::ostream &out, const Student &st) {
+    out << "Studentul este: " << st.name << std::endl;
+    out << "Studentul are cnp: " << st.cnp << std::endl;
+    out << "Studentul are media: "<<st.average<<std::endl;
     return out;
 }
-istream  &operator>>(istream &is,  Student &st) {
-    cout << "Studentul este: " ;
-    is >> st.nume;
-    cout << "Studentul are cnp: ";
+std::istream  &operator>>(std::istream &is,  Student &st) {
+    std::cout << "Studentul este: " ;
+    is >> st.name;
+    std::cout << "Studentul are cnp: ";
     is >> st.cnp;
-    cout << "Studentul are media: ";
-    is >> st.medie;
+    std::cout << "Studentul are media: ";
+    is >> st.average;
     return is;
 }
 class Facultate{
 private:
-    string nume;
-    int numarprofesori;
-    int numarmaterii;
-    int numarstudenti;
+    std::string name;
+    int numberteachers;
+    int numberobjects;
+    int numberstudents;
 public:
-    ~Facultate(){cout<<"A fost apelat \n";}
-    explicit Facultate(const string &nume="n/a",int numarprofesori=0, int numarmaterii=0,int numarstudenti=0){
-        this->nume=nume;
-        this->numarmaterii=numarmaterii;
-        this->numarprofesori=numarprofesori;
-        this->numarstudenti=numarstudenti;
+    ~Facultate(){std::cout<<"A fost apelat \n";}
+    explicit Facultate(const std::string &local_name="n/a",int local_numberteachers=0, int local_numberobjects=0,int local_numberstudents=0){
+        this->name=local_name;
+        this->numberobjects=local_numberobjects;
+        this->numberteachers=local_numberteachers;
+        this->numberstudents=local_numberstudents;
     }
-    string getnume() const {
-        return this->nume;
+    std::string getname() const {
+        return this->name;
     }
 
-    void setnume(const string &name) {
-        this->nume = name;
+    void setnume(const std::string &local_name) {
+        this->name = local_name;
     }
     int getnumarprofesori ()const{
-        return this->numarprofesori;
+        return this->numberteachers;
     }
     int setnumarprofesori(int x) {
-        return this->numarprofesori=x;
+        return this->numberteachers=x;
     }
     int getnumarmaterii ()const{
-        return this->numarmaterii;
+        return this->numberobjects;
     }
     int setnumarmaterii(int x) {
-        return this->numarmaterii=x;
+        return this->numberobjects=x;
     }
     int getnumarstudenti ()const{
-        return this->numarstudenti;
+        return this->numberstudents;
     }
     int setnumarstudenti(int x) {
-        return this->numarstudenti=x;
+        return this->numberstudents=x;
     }
-    friend ostream &operator<<(ostream &out, const Facultate &fc);
-    friend istream &operator>>(istream &is, Facultate &fc);
+    friend std::ostream &operator<<(std::ostream &out, const Facultate &fc);
+    friend std::istream &operator>>(std::istream &is, Facultate &fc);
 };
-ostream &operator<<(ostream &out, const Facultate &fc) {
-    out << "Facultatea are numele: "<< fc.nume<<endl;
-    out << "Numarul studentilor este: " << fc.numarstudenti << endl;
-    out << "Numarul materiilor este: " << fc.numarmaterii << endl;
-    out << "Numarul profesorilor din facultate este: "<<fc.numarprofesori<<endl;
+std::ostream &operator<<(std::ostream &out, const Facultate &fc) {
+    out << "Facultatea are numele: "<< fc.name<<std::endl;
+    out << "Numarul studentilor este: " << fc.numberstudents << std::endl;
+    out << "Numarul materiilor este: " << fc.numberobjects << std::endl;
+    out << "Numarul profesorilor din facultate este: "<<fc.numberteachers<<std::endl;
     return out;
 }
 
-istream  &operator>>(istream &is,  Facultate &fc) {
-    cout << "Facultatea are numele: ";
-    is>>fc.nume;
-    cout << "Numarul studentilor este: ";
-    is>>fc.numarstudenti;
-    cout << "Numarul materiilor este: ";
-    is>>fc.numarmaterii;
-    cout << "Numarul profesorilor din facultate este: ";
-    is>>fc.numarprofesori;
+std::istream  &operator>>(std::istream &is,  Facultate &fc) {
+    std::cout << "Facultatea are numele: ";
+    is>>fc.name;
+    std::cout << "Numarul studentilor este: ";
+    is>>fc.numberstudents;
+    std::cout << "Numarul materiilor este: ";
+    is>>fc.numberobjects;
+    std::cout << "Numarul profesorilor din facultate este: ";
+    is>>fc.numberteachers;
     return is;
 }
 int main() {
-    vector <Student>st;
-    st.reserve(3);
     Materie m1("Chimie", "Ionescu");
-    cout << m1;
+    std::cout << m1;
     Materie m2;
-    m2.setnume("Analiza");
-    m2.setprofesor("Mircea");
-    cout << "Materia este : " << m2.getnume() << " \n";
-    cout << "Materia este predata de : " << m2.getprofesor() << " \n";
-    cout << endl << endl;
+    m2.setname("Analiza");
+    m2.setteacher("Mircea");
+    std::cout << "Materia este : " << m2.getname() << " \n";
+    std::cout << "Materia este predata de : " << m2.getteacher() << " \n";
+    std::cout << std::endl << std::endl;
     Student s1("Alex","1234567890123",8.3);
-    cout << s1;
+    std::cout << s1;
     Student s2;
-    s2.setnume("Mihai");
+    s2.setname("Mihai");
     s2.setcnp("5082710481244");
-    s2.setmedie(8.56);
-    cout << "Numele studentului este : "<< s2.getnume()<< "\n";
-    cout << "Studentul are CNP: "<< s2.getcnp()<<"\n";
-    cout << "Studentul are media: "<< s2.getmedie()<<"\n";
+    s2.setaverage(8.56);
+    std::cout << "Numele studentului este : "<< s2.getname()<< "\n";
+    std::cout << "Studentul are CNP: "<< s2.getcnp()<<"\n";
+    std::cout << "Studentul are media: "<< s2.getaverage()<<"\n";
     Student s3;
     s3=s2;
-    cout << s3;
-    cout << endl << endl;
+    std::cout << s3;
+    std::cout << std::endl << std::endl;
     Facultate f1("Facultatea de matematica si informatica",2,2,2);
-    cout << f1;
+    std::cout << f1;
     Facultate f2;
     f2.setnume ("Facultatea de Constructii Bucuresti");
     f2.setnumarmaterii (3);
     f2.setnumarprofesori (2);
     f2.setnumarstudenti (60);
-    cout<< "Numele: "<< f2.getnume()<<endl;
-    cout<< "Numarul profesorilor: "<< f2.getnumarprofesori()<<endl;
-    cout<< "Numarul materiilor: "<< f2.getnumarmaterii()<<endl;
-    cout<< "Numarul studentiilor: "<< f2.getnumarstudenti()<<endl;
-    cout << endl << endl;
-
-    st.emplace_back(s1);
-    st.emplace_back(s2);
-    st.emplace_back(s3);
-    st.reserve(3);
-   /* sort(st.begin(),st.end(),[](const Student& s1,const Student&s2){
-        if (s1.getmedie()==s2.getmedie()) {
-            if (s1.getnume() == s2.getnume()) {
-                return s1.getcnp() < s2.getcnp();
-            }
-            return s1.getnume() <s2.getnume();
-        }
-        return s1.getmedie() > s2.getmedie();
-    });*/
-    cout << endl;
-    for (auto& studenti: st){
-        cout<<studenti;
-    }
+    std::cout<< "Numele: "<< f2.getname()<<std::endl;
+    std::cout<< "Numarul profesorilor: "<< f2.getnumarprofesori()<<std::endl;
+    std::cout<< "Numarul materiilor: "<< f2.getnumarmaterii()<<std::endl;
+    std::cout<< "Numarul studentiilor: "<< f2.getnumarstudenti()<<std::endl;
+    std::cout << std::endl << std::endl;
     return 0;
 }
